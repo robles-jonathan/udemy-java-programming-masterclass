76044bfb1099dffed52d8d7406eab76f7972cb5a
Jonathan Robles <jonathan.robles287@gmail.com>
2022-04-13 13:40:39 -0500

    Strings

---
+++ src/fourth/CharAndBoolean.java
@@ -0,0 +1,22 @@
+package fourth;
+
+public class CharAndBoolean {
+    public static void main(String[] args) {
+        
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+
+        boolean isCustomerOverTwentyOne = true;
+        System.out.println(isCustomerOverTwentyOne);
+
+        
+    }
+}
