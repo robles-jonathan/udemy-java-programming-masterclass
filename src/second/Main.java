ea0187ccc45625581c6caff6655ea8437094a301
Jonathan Robles <jonathan.robles287@gmail.com>
2022-04-13 11:40:57 -0500

    primitive types

---
+++ src/second/Main.java
@@ -0,0 +1,18 @@
+package second;
+
+public class Main {
+    public static void main(String[] args) {
+        int myValue = 1000;
+        int myMinValue = Integer.MIN_VALUE;
+        int myMaxValue = Integer.MAX_VALUE;
+        System.out.println("Integer Min Value = " + myMinValue);
+        System.out.println("Integer Max Value = " + myMaxValue);
+        System.out.println("Busted Max Value = " + (myMaxValue + 1));
+        System.out.println("Busted Min Value = " + (myMinValue - 1));
+        int myMaxIntTest = 2_147_483_647;
+        
+        
+    }
+    
+    
+}
