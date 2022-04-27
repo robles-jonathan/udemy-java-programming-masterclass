cada84adc8de752a0b77d0a509fc9769d6a9bef5
Jonathan Robles <jonathan.robles287@gmail.com>
2022-04-26 22:56:35 -0500

    Went over What is an expression

---
+++ src/seventh/keywordsAndExpressions.java
@@ -0,0 +1,17 @@
+package seventh;
+
+public class keywordsAndExpressions {
+    public static void main(String[] args) {
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score");
+            score = 0;
+        }
+    }
+    // write down the parts of the code that are expressions
+    // score = 100
+    // score > 99
+    // "You got the high score"
+    // score = 0
+
+}
