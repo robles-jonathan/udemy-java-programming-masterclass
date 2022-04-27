95f5f9dd8dcf1c5f927ff401e73b6052743cb892
Jonathan Robles <jonathan.robles287@gmail.com>
2022-04-27 17:29:49 -0500

    Method return statements simplified

--- src/section2/codeBlocks.java
+++ src/section2/codeBlocks.java
@@ -45,15 +45,25 @@ public class codeBlocks {
     }
 
     public static int calculateHighScorePosition(int score){
+        // if(score >= 1000){
+        //     return 1;
+        // } else if(score >= 500){
+        //     return 2;
+        // } else if(score >= 100){
+        //     return 3;
+        // }
+        
+        // return 4;
+
+        int position = 4; // assuming position 4 will be returned as
         if(score >= 1000){
-            return 1;
-        } else if(score >= 500){
-            return 2;
-        } else if(score >= 100){
-            return 3;
-        } else{
-            return 4;
-        }
+                position = 1;
+            } else if(score >= 500){
+                position = 2;
+            } else if(score >= 100){
+                position = 3;
+            }
+            return position;
     }
 
 }
