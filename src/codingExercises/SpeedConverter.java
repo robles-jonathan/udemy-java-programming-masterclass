fff131e6a74a6f57b5c471788d742d692137edd1
Jonathan Robles <jonathan.robles287@gmail.com>
2022-04-28 16:58:28 -0500

    Created toMilesPerHour method.

---
+++ src/codingExercises/SpeedConverter.java
@@ -0,0 +1,12 @@
+package codingExercises;
+
+public class SpeedConverter {
+    
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if(kilometersPerHour < 0){
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609);
+    }
+}
