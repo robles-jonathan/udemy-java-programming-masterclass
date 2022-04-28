6b2b137eb62997638194881233f18ee6d8e05a48
Jonathan Robles <jonathan.robles287@gmail.com>
2022-04-28 17:10:08 -0500

    Tested functions in Main.java

--- src/codingExercises/SpeedConverter.java
+++ src/codingExercises/SpeedConverter.java
@@ -9,4 +9,13 @@ public class SpeedConverter {
 
         return Math.round(kilometersPerHour / 1.609);
     }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            System.out.println("Invalid value.");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
 }
