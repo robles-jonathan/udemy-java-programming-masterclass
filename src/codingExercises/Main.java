2c32d9de360289ebbb440b05eb9fcbf06e85ec1d
Jonathan Robles <jonathan.robles287@gmail.com>
2022-05-01 20:44:01 -0500

    Finsihed exercise 5.

--- src/codingExercises/Main.java
+++ src/codingExercises/Main.java
@@ -13,23 +13,20 @@ public class Main {
         // System.out.println(BarkingDog.shouldWakeUp(true, 8));
         // System.out.println(BarkingDog.shouldWakeUp(true, -1));
 
-        LeapYear.isLeapYear(1700);
-        LeapYear.isLeapYear(1800);
-        LeapYear.isLeapYear(1900);
-        LeapYear.isLeapYear(2100);
-        LeapYear.isLeapYear(2200);        
-        LeapYear.isLeapYear(2300);
-        LeapYear.isLeapYear(2600);
-        LeapYear.isLeapYear(1600);
-        LeapYear.isLeapYear(2000);
-        LeapYear.isLeapYear(2400);
-        LeapYear.isLeapYear(1600);
-        LeapYear.isLeapYear(2017);
-        LeapYear.isLeapYear(2000);
+        // LeapYear.isLeapYear(1700);
+        // LeapYear.isLeapYear(1800);
+        // LeapYear.isLeapYear(1900);
+        // LeapYear.isLeapYear(2100);
+        // LeapYear.isLeapYear(2200);        
+        // LeapYear.isLeapYear(2300);
+        // LeapYear.isLeapYear(2600);
+        // LeapYear.isLeapYear(1600);
+        // LeapYear.isLeapYear(2000);
+        // LeapYear.isLeapYear(2400);
+        // LeapYear.isLeapYear(1600);
+        // LeapYear.isLeapYear(2017);
+        // LeapYear.isLeapYear(2000);
 
-
-
-
-
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.1756));
     }
 }
